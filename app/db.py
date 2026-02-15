318c70895b8755c48591e2c3532ca46b8c569342
Sajad Karami <ajadkarami@itmanagers-MacBook-Air.local>
2026-02-15 17:19:14 +0330

    Make database persistant

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Boolean, Column, Integer, String, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./data.db"
+DATABASE_URL = "sqlite:///./data/data.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
